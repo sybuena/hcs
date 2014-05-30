7c094c548955f2be306b7f06e7aca1595a840241
Christian Buenavista <Christian@propertyinabox.com.au>
2014-05-31 03:50:22 +0800

    forgot backgroud mode

--- js/app.js
+++ js/app.js
@@ -1251,7 +1251,7 @@ document.addEventListener('deviceready', function() {
 	
 
 	//Enables the background mode. The app will not pause while in background.
-	//window.plugin.backgroundMode.enable();
+	window.plugin.backgroundMode.enable();
 	
 	var params = [];
 	
