dca3d9e9500833163b054424867112d79a92edd7
Christian Buenavista <Christian@propertyinabox.com.au>
2014-06-03 05:28:51 +0800

    iscroll

--- js/app.js
+++ js/app.js
@@ -43,6 +43,118 @@ window.start 			= false;
 window.username 		= localStorage.getItem('username');
 window.password 		= localStorage.getItem('password');
 
+var myScroll,
+	pullDownEl, pullDownOffset,
+	pullUpEl, pullUpOffset,
+	generatedCount = 0;
+
+
+function pullDownAction () {
+	
+	//get the active listing
+	var type 	= $('ul.nav-stacked li.active a.left-navigation').attr('id');
+	var start 	= 10;
+	var end  	= 0;
+    
+    //now make request to backend
+    window.messages.checkInbox(type, 1);
+	
+	window.messageList[type] = _string.unlock(type);		
+	
+	//Remember to refresh when contents are loaded (ie: on ajax completion)
+	myScroll.refresh();		
+	
+}
+
+function pullUpAction () {
+	
+	var type = $('ul.nav-stacked li.active a.left-navigation').attr('id');
+	var start = 10;
+	var count = 11;
+
+	start = start = 10;
+	count++;
+		
+	window.messages.pullDown(window.messageList[type], type, start, 0);	
+	
+	//On click message listing then load message detail
+	//base on Message GUID
+	onClickDetail(type);
+
+	myScroll.refresh();		// Remember to refresh when contents are loaded (ie: on ajax completion)
+	
+}
+
+function loaded() {
+	pullDownEl 		= document.getElementById('pullDown');
+	pullDownOffset 	= pullDownEl.offsetHeight;
+	pullUpEl 		= document.getElementById('pullUp');	
+	pullUpOffset 	= pullUpEl.offsetHeight;
+	myScroll 		= new iScroll('wrapper', {
+		useTransition 	: true,
+		topOffset 		: pullDownOffset,
+		onRefresh 		: function () {
+			if (pullDownEl.className.match('loading')) {
+				pullDownEl.className = '';
+				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
+			} else if (pullUpEl.className.match('loading')) {
+				pullUpEl.className = '';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Pull up to load more...';
+			}
+		},
+		onScrollMove 	: function () {
+			if (this.y > 5 && !pullDownEl.className.match('flip')) {
+				pullDownEl.className = 'flip';
+				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Release to refresh...';
+				this.minScrollY = 0;
+			} else if (this.y < 5 && pullDownEl.className.match('flip')) {
+				pullDownEl.className = '';
+				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
+				this.minScrollY = -pullDownOffset;
+			} else if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+				pullUpEl.className = 'flip';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Release to refresh...';
+				this.maxScrollY = this.maxScrollY;
+			} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+				pullUpEl.className = '';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Pull up to load more...';
+				this.maxScrollY = pullUpOffset;
+			}
+		},
+		onScrollEnd 	: function () {
+			if (pullDownEl.className.match('flip')) {
+				pullDownEl.className = 'loading';
+				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Loading...';				
+				pullDownAction();	// Execute custom function (ajax call?)
+			} else if (pullUpEl.className.match('flip')) {
+				pullUpEl.className = 'loading';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Loading...';				
+				pullUpAction();	// Execute custom function (ajax call?)
+			}
+		}
+	});
+	
+	setTimeout(function () { 
+		document.getElementById('wrapper').style.left = '0'; 
+	}, 800);
+}
+
+
+/**
+ * Fixed for IOS position : fixed
+ *
+ */
+document.addEventListener('touchmove', function (e) { 
+	e.preventDefault(); 
+}, false);
+
+/**
+ * If all DOM files are loaded
+ *
+ */
+document.addEventListener('DOMContentLoaded', function () { 
+	setTimeout(loaded, 200); 
+}, false);
 
 function init() {
 	/* -----------------------------------------
@@ -142,7 +254,7 @@ function bind() {
 
 	  	
 		//pull to refresh 		
-		pullRefresh();	
+		//pullRefresh();	
 
 		$('.page-sidebar').css('background-color','white');
 		$('.page-sidebar').css('height','100%');
@@ -1336,6 +1448,8 @@ document.addEventListener('deviceready', function() {
 		$('.notification-ajax #notification-here').html('<i class="fa fa-warning"></i> No Internet connection');
 		
 	});
+
+	
 
 
 	$(document).ready(function(){
